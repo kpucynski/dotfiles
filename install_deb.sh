d34f24278ff9a29d5e79e73c28cf755b9ee7d496
Karol Pucynski <kpucynski@gmail.com>
2019-11-04 14:52:25 +0100

    updates

--- install_deb.sh
+++ install_deb.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+# it should be done in Ansible
+
 sudo apt install pandoc vim mc gnome-tweaks diodon
 
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+
+sudo add-apt-repository ppa:linuxuprising/shutter
+sudo apt-get update && sudo apt-get install shutter
+
+sudo apt install lastpass-cli
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
