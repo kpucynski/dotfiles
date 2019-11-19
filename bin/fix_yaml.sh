2cb24ad31b8f57b0ce99bd13e8998b266db86a6c
Karol Pucynski <kpucynski@gmail.com>
2019-11-20 00:07:43 +0100

    fix

--- bin/fix_yaml.sh
+++ bin/fix_yaml.sh
@@ -2,17 +2,17 @@
 
 while IFS= read -r -d '' file
 do
-  echo $file
+  echo "${file}"
   
   # fix header
-  if [ ! "$(head -1 $file)" == "---" ]; then
-  	sed -i '1s,^,---\n,' $file;
+  if [ ! "$(head -1 "${file}")" == "---" ]; then
+  	sed -i '1s,^,---\n,' "${file}";
   fi;
   
   # fix trailing spaces
-  sed -i 's/[ \t]\+$//' $file;
+  sed -i 's/[ \t]\+$//' "${file}";
   
   # fix comments
-  sed -i 's/^\([[:space:]]*\)#\([[:alpha:]]\)/\1# \2/g' $file
+  sed -i 's/^\([[:space:]]*\)#\([[:alpha:]]\)/\1# \2/g' "${file}"
 
-done < <(find $1 \( -name "*.yml" -o -name "*.yaml" \) -print0)
+done < <(find . \( -name "*.yml" -o -name "*.yaml" \) -print0)
