d8fe7125915580e41e476f58b3e56016aa610078
Karol Pucynski <kpucynski@gmail.com>
2020-04-30 01:33:11 +0200

    deepin-screenshot

--- install_deb.sh
+++ install_deb.sh
@@ -12,12 +12,12 @@ curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update && sudo apt-get install spotify-client
 
-sudo add-apt-repository ppa:linuxuprising/shutter
+#sudo add-apt-repository ppa:linuxuprising/shutter
 sudo add-apt-repository ppa:phoerious/keepassxc
 
 sudo apt install \
   gnome-clocks \
-  shutter \
+  deepin-screenshot \
   virtualenv \
   keepassxc \
   lastpass-cli \
